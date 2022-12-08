26939d01a9cc4775dbe5683275042b6e94600c79
Alexandre BG <alexandre.brungiglio@gmail.com>
2022-12-08 13:58:08 +0100

    customize layout for admin or unlogged user

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,54 @@
-const Layout: React.FC = () => {
+import React from 'react';
+import * as Icon from "react-icons/io";
+import { useRoute } from '../../routes/route-provider';
+import './layout.scss';
+import NavbarMinimal from './Navigation';
+import { Nav } from './PublicNav';
+
+
+interface props {
+    children: React.ReactNode;
+}
+
+const Layout = ({ children }: props) => {
+    const { currentRoute } = useRoute();
+
     return (
-        <></>
+        <>
+            {
+                currentRoute.includes('admin')
+                    ? <AdminLayout>{children}</AdminLayout>
+                    :
+                    <PublicLayout>{children}</PublicLayout>
+            }
+        </>
     );
 };
 export default Layout;
+
+
+const AdminLayout = ({ children }: props) => {
+    return (
+        <div className='layout-admin'>
+            <div className='menu'>
+                <NavbarMinimal />
+            </div>
+            <div className='content'>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+const PublicLayout = ({ children }: props) => {
+    return (
+        <div className='layout-public'>
+            <div className='menu'>
+                <Nav />
+            </div>
+            <div className='content'>
+                {children}
+            </div>
+        </div>
+    );
+};
--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,6 +1,84 @@
-const Navigation: React.FC = () => {
+import { useState } from 'react';
+import { Navbar, Center, Tooltip, UnstyledButton, createStyles, Stack } from '@mantine/core';
+import * as Fa from 'react-icons/fa';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { RoutesList, RoutesListWrapper } from '../../routes/routes';
+import { useRoute } from '../../routes/route-provider';
+
+
+const useStyles = createStyles((theme) => ({
+    link: {
+        width: 50,
+        height: 50,
+        borderRadius: theme.radius.md,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+
+        '&:hover': {
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
+        },
+    },
+
+    active: {
+        '&, &:hover': {
+            backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
+            color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+        },
+    },
+}));
+
+interface NavbarLinkProps {
+    icon: any;
+    label: string;
+    active?: boolean;
+    onClick?(): void;
+}
+
+function NavbarLink({ icon: Icon, label, active, onClick }: NavbarLinkProps) {
+    const { classes, cx } = useStyles();
     return (
-        <></>
+        <Tooltip label={label} position="right" transitionDuration={0}>
+            <UnstyledButton onClick={onClick} className={cx(classes.link, { [classes.active]: active })}>
+                <Icon stroke={1.5} />
+            </UnstyledButton>
+        </Tooltip>
     );
-};
-export default Navigation;
+}
+
+
+export function NavbarMinimal() {
+    const [active, setActive] = useState(2);
+    const { getAdminRoutes } = useRoute();
+    const mockdata = getAdminRoutes();
+
+    const links = mockdata.map((link, index) => (
+        <NavbarLink
+            {...link}
+            key={link.label}
+            active={index === active}
+            onClick={() => setActive(index)}
+        />
+    ));
+
+    return (
+        <Navbar height={750} width={{ base: 80 }} p="md">
+            <Center>
+                <img style={{ height: '50px', width: '50px' }} src='/img/alx.png' alt='logo_alx' />
+            </Center>
+            <Navbar.Section grow mt={50}>
+                <Stack justify="center" spacing={0}>
+                    {links}
+                </Stack>
+            </Navbar.Section>
+            <Navbar.Section>
+                <Stack justify="center" spacing={0}>
+                    <NavbarLink icon={AiOutlineLogout} label="Déconnexion" />
+                </Stack>
+            </Navbar.Section>
+        </Navbar>
+    );
+}
+
+export default NavbarMinimal;
